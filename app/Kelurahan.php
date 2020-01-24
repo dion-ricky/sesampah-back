c556223bae65fe4c019eb86df523ee05a6a977da
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-24 13:23:36 +0700

    Configure kelurahan model

--- app/Kelurahan.php
+++ app/Kelurahan.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Kelurahan extends Model
 {
     //
+    protected $fillable = [
+        'kecamatan_id',
+        'kelurahan',
+        'kode_pos'
+    ];
 }
