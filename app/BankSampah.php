047086d2d9802603398519fa1b38297ce15e0b61
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:31:47 +0700

    Configured bank sampah model

--- app/BankSampah.php
+++ app/BankSampah.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class BankSampah extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'no_telp',
+        'alamat',
+        'kelurahan_id',
+    ];
 }
