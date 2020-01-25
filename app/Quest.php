129680955115d4d63b82a066c0acd4939dae6547
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 09:37:23 +0700

    Configured quest model

--- app/Quest.php
+++ app/Quest.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Quest extends Model
 {
     //
+    protected $fillable = [
+        'cash_reward',
+        'point_reward',
+        'xp_reward',
+        'start_date',
+        'deadline'
+    ];
 }
