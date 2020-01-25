0bed1bbbabe199e51c5c2f11f9aaa1ee59fb2aff
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:13:49 +0700

    Configured user game item model

--- app/UserGameItem.php
+++ app/UserGameItem.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class UserGameItem extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'game_item_id',
+        'received_date',
+    ];
 }
