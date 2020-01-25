b4b6608902f98a6f2087c59d8df6ea0f693581dd
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:18:08 +0700

    Configured user quest model

--- app/UserQuest.php
+++ app/UserQuest.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class UserQuest extends Model
 {
     //
+    protected $fillable = [
+        'quest_id',
+        'user_id',
+        'status',
+        'progress',
+        'start_date',
+        'finish_date',
+    ];
 }
