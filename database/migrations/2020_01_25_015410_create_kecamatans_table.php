95012d624ca10ab6861418f567d4019523c456b2
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 08:57:26 +0700

    Configured kecamatan migration

---
+++ database/migrations/2020_01_25_015410_create_kecamatans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKecamatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kecamatans', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->smallInteger('kota_id')->references('id')->on('kota')->nullable();
+            $table->string('kecamatan', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kecamatans');
+    }
+}
