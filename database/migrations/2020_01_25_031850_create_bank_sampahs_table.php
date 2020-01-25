cbee00ada6f12112b63248fa22e940bd7dd90ded
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:31:55 +0700

    Configured bank sampah migration

--- database/migrations/2020_01_25_031850_create_bank_sampahs_table.php
+++ database/migrations/2020_01_25_031850_create_bank_sampahs_table.php
@@ -15,6 +15,10 @@ class CreateBankSampahsTable extends Migration
     {
         Schema::create('bank_sampah', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('no_telp', 25);
+            $table->string('alamat', 50);
+            $table->smallInteger('kelurahan_id')->references('id')->on('kelurahan');
             $table->timestamps();
         });
     }
