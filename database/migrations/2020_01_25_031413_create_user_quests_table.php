70050e32d975b32fef78b92451d96f8148e258c4
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:18:16 +0700

    Configured user  quest migration

--- database/migrations/2020_01_25_031413_create_user_quests_table.php
+++ database/migrations/2020_01_25_031413_create_user_quests_table.php
@@ -14,8 +14,14 @@ class CreateUserQuestsTable extends Migration
     public function up()
     {
         Schema::create('user_quests', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->integer('quest_id')->references('id')->on('quests');
+            $table->integer('user_id')->references('id')->on('users');
+            $table->boolean('status');
+            $table->smallInteger('progress');
+            $table->date('start_date');
+            $table->date('finish_date');
             $table->timestamps();
+            $table->index(['quest_id', 'user_id']);
         });
     }
 
