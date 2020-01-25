079e48149f01ed134baf9d37a2e44ae35fb9b331
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:08:54 +0700

    Configured UserVouchers model

--- app/UserVouchers.php
+++ app/UserVouchers.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class UserVouchers extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'voucher_id',
+        'is_valid',
+        'received_date',
+        'used_date',
+    ];
 }
