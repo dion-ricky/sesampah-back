91723aad274fe4e064af8ecd19d6359c8d3972c9
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:37:43 +0700

    Configured transaksi model

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     //
+    protected $fillable = [
+        'bank_sampah_id',
+        'user_id',
+        'is_incoming',
+        'is_money',
+        'amount',
+    ];
 }
