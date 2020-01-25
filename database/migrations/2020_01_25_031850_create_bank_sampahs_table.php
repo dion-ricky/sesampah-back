48b7b6e8037c7535d420635ff79c8386c465967d
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 10:19:18 +0700

    Added bank sampah model and migration

---
+++ database/migrations/2020_01_25_031850_create_bank_sampahs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankSampahsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_sampah', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_sampah');
+    }
+}
