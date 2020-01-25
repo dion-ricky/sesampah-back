5db1b6ee80194f1eecea7317e9f5e765427ae199
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 09:01:55 +0700

    Configured provinsi migration

--- database/migrations/2020_01_25_020002_create_provinsis_table.php
+++ database/migrations/2020_01_25_020002_create_provinsis_table.php
@@ -14,8 +14,8 @@ class CreateProvinsisTable extends Migration
     public function up()
     {
         Schema::create('provinsis', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->smallIncrements('id');
+            $table->string('provinsi', 50);
         });
     }
 
