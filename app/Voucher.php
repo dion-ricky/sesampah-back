5eb3325235be19481595b0e5861cfc9f6c0f82e8
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-01-25 09:27:37 +0700

    Configured voucher model

--- app/Voucher.php
+++ app/Voucher.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Voucher extends Model
 {
     //
+    protected $fillable = [
+        'sponsor_id',
+        'asset_id',
+        'title',
+        'description',
+        'available_from',
+        'valid_until',
+    ];
 }
