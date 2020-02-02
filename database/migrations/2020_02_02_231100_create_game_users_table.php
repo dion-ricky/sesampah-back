8ffb8711d0b964747e3ef9fa6369bf1a28b77767
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-02-03 06:15:32 +0700

    Added and configured GameUser model and migration

---
+++ database/migrations/2020_02_02_231100_create_game_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_users', function (Blueprint $table) {
+            $table->bigIteger('user_id')->references('users')->on('id');
+            $table->smallInteger('level')->default(1);
+            $table->integer('xp')->default(0);
+            $table->unsignedTinyInteger('tier')->default(1);
+            $table->timestamps();
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_users');
+    }
+}
