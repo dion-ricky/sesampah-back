1891bc505a54d97761cbbc767ce6ce978c54eba9
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-02-03 06:10:12 +0700

    Added and configured Mitra model and migration

---
+++ database/migrations/2020_02_02_230236_create_mitras_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMitrasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mitras', function (Blueprint $table) {
+            $table->bigInteger('user_id')->references('users')->on('id');
+            $table->smallInteger('level')->default(1);
+            $table->integer('xp')->default(0);
+            $table->unsignedTinyInteger('tier')->default(1);
+            $table->timestamps();
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mitras');
+    }
+}
