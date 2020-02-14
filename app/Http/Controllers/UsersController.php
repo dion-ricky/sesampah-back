291a913c53e8486c4705c88d6be0a766dcdf049d
Dion Ricky Saputra <dionrickysptr@gmail.com>
2020-02-15 00:40:29 +0700

    Added api resource functions on Users Controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserResourceCollection;
 
 class UsersController extends Controller
 {
     //
+    public function index(): UserResourceCollection {
+        return new UserResourceCollection(User::paginate());
+    }
+
+    public function show(User $user): UserResource {
+        return new UserResource($user);
+    }
+
+    public function store(Request $request) {
+        // dd($request->all());
+
+        $request->validate([
+            'user_uid' => 'required',
+            'username' => 'required',
+            'name' => 'required',
+            'email' => 'required'
+        ]);
+
+        $user = User::create($request->all());
+
+        return new UserResource($user);
+    }
+
+    public function update(User $user, Request $request): UserResource {
+        $user->update($request->all());
+
+        return new UserResource($user);
+    }
+
+    public function destroy(User $user) {
+        $user->delete();
+
+        return response()->json();
+    }
 }
